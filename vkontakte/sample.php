01737ed26247192c3fd7d8bad5a3538ccac99878
Omaroviz <omaroviz1990@gmail.com>
2026-06-28 21:38:16 +0300

    GUI search.php IN sample.php

--- vkontakte/sample.php
+++ vkontakte/sample.php
@@ -40,12 +40,38 @@ session_start();
 	</div>
 
 </header>
+<div style="display: flex; flex-direction: column; align-items: center; width: 100%;">
+<main class="search_main">
 
-
-<main class="page-layout">
-<div class="post">
+<?php
+$sql = "SELECT * FROM users WHERE id = :text OR name = :text OR username = :text";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([':text' => "stepa"]);
+$users = $stmt->fetchAll();
+foreach ($users as $user) {
+	echo <<<_END
+<div class="search_box">
+<span class="search_info"> 
+<span class="search_avatar">MH</span>
+<span class="search_info_text">
+<span class="search_name">
+{$user['name']}
+</span>
+<span class="search_username">
+@{$user['username']}
+</span>
+</span>
+</span>
+<span class="search_right">
+<a class="search_write_a">Написать</a>
+<a class="search_write_a">Добавить</a>
+</span>
 </div>
+_END;
+}
+?>
 </main>
+</div>
 <footer id="vniz" style="margin: 0;">
 	<a href="#" class="vk-button" onclick="confirm('О сайте.\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nПривет')">О сайте</a>
 	"Вконтакте Druza" ООО. Все права защищены. Распространения кода сайта разрешено с <a href="#">условиями
